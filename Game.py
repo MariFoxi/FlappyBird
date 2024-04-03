cede779546d3621c562645a1c48e3f7734703e6d
Vagosina <150610412+Vagosina@users.noreply.github.com>
2024-04-03 11:04:02 +0300

    Game.py

--- Game.py
+++ Game.py
@@ -1,3 +1,24 @@
-import math
+class Game:
+  def __init__(self) -> None:
+  pg.init()
 
-print("Start_of_project")
+  # размеры экрана
+  self.screen_w = 400
+  self.screen_h = 800
+
+  # размеры труб
+  self.pipe_w = 80
+  self.pipe_h = 500
+
+  self.pipe_gap = 180  # расстояние между трубами
+
+  # размер земли
+  self.ground_w = 2 * self.screen_w
+  self.ground_h = 100
+
+  self.speed = 10  # скорость птички
+  self.gravity = 1  # гравитация
+
+  # задаём окно и название
+  self.screen = pg.display.set_mode((self.screen_w, self.screen_h))
+  pg.display.set_caption('Flappy Bird')
