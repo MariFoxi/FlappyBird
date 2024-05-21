c3730a26ac02af99b10afad78e91083b430fc9b2
Vagosina <150610412+Vagosina@users.noreply.github.com>
2024-05-21 23:15:18 +0300

    Update Ground.py

--- Ground.py
+++ Ground.py
@@ -1,2 +1,22 @@
-def init(self, root, x_pos: int) -> None:
-  pass
+import pygame as pg
+class Ground(pg.sprite.Sprite):
+    def __init__(self, root, x_pos: int) -> None:
+        # инициализируем Sprite
+        super().__init__()
+        self.game = root  # поле игры
+
+        # грузим и растягиваем изображение
+        self.image = pg.transform.scale(pg.image.load('base.png'),
+                                        (self.game.ground_w, self.game.ground_h))
+        # конвертируем изображение
+        self.image = self.image.convert_alpha()
+
+        # создаём маску по картинке
+        self.mask = pg.mask.from_surface(self.image)
+
+        # создаём хитбокс, который расположем в точке x_pos и по y внизу экрана
+        self.rect = self.image.get_rect(x=x_pos,
+                                        y=self.game.screen_h - self.game.ground_h)
+
+    def update(self) -> None:
+        self.rect.x -= self.game.speed
